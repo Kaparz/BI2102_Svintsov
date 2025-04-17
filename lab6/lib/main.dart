d3a1fa07111853447455d4313d143df8d2500c37
Kaparz <nya1234567890q@mail.ru>
2025-04-18 00:36:19 +0300

    Update main.dart

--- lab6/lib/main.dart
+++ lab6/lib/main.dart
@@ -12,7 +12,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Калькулятор площади',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.green,
       ),
       home: const AreaCalculator(),
     );
